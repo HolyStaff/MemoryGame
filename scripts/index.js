df7e1e3a7886a4d20568af54b11e67b558e31308
Hael <86430628+HolyStaff@users.noreply.github.com>
2025-05-07 17:04:37 +0200

    Braindead memory, kan tiles matchen

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let tiles = [];
+
+function flipTile(cell) {
+    if (cell.style.backgroundColor === "green") {
+        return;
+    }
+    cell.style.backgroundColor = "red";
+    checkForMatch(cell);
+}
+
+function checkForMatch(cell) {
+    if (tiles.length < 2) {
+        tiles.push(cell);
+        if (tiles.length === 2) {
+            if (tiles[0].textContent === tiles[1].textContent) {
+                tiles[0].style.backgroundColor = "green";
+                tiles[1].style.backgroundColor = "green";
+                tiles = [];
+                return;
+            }
+            setTimeout(() => {
+                tiles[0].style.backgroundColor = "white";
+                tiles[1].style.backgroundColor = "white";
+                tiles = [];
+            }, 1000);
+        }
+    }
+}
