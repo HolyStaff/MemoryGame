7d93faaac1b874b200c2d738c416e56d0ac06250
Hael <86430628+HolyStaff@users.noreply.github.com>
2025-06-16 15:07:49 +0200

    Added API, timer, split up js scripts in modules

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,5 @@
-import * as tile from "./tile_functionality";
+import MemoryGame from './game.js';
 
-let boardSize = 4;
-
-
-
-document.addEventListener("DOMContentLoaded", generateBoard); // Generate a board when the page is loaded
-
-function generateBoard(){
-    for(let i = 0; i < boardSize**2; i++){
-        tile.create();
-    }
-}
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    new MemoryGame();
+});
