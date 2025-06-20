0cedf5af4055bd8cbe9012432580e6ac4f68333e
Hael <86430628+HolyStaff@users.noreply.github.com>
2025-06-20 17:03:49 +0200

    port change

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'http://localhost:8000/api';
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
